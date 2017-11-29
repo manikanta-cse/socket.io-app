be83acad4e8d60d073ca3834d1bfda97eecea56c
Kattamuri, Manikanta <Manikanta.Kattamuri@cdk.com>
2017-11-29 11:24:21 +0530

    final commit

--- public/main/main.Ctrl.js
+++ public/main/main.Ctrl.js
@@ -8,6 +8,90 @@
     MainCtrl.$inject = ['$scope', '$localStorage', 'socket', 'lodash'];
   
     function MainCtrl($scope, $localStorage, socket, lodash) {
-  
+      $scope.message="";
+      $scope.messages=[];
+      $scope.users=[];
+      $scope.likes=[];
+      $scope.mynickname=$localStorage.nickname;
+      var nickname=$scope.mynickname;
+
+
+      socket.emit('get-users');
+
+      socket.on('all-users',function(data){
+        console.log(data);
+
+        $scope.users=data.filter(function(item){
+          return item.nickname!==nickname;
+        });
+      });
+
+
+      socket.on('message-recieved',function(data){
+        $scope.messages.push(data);
+      });
+
+      socket.on('user-liked',function(data){
+        console.log(data);
+        $scope.likes.push(data.from);
+      });
+
+
+      socket.on('show-message',function(data){
+        console.log(data);
+      });
+
+
+
+      $scope.sendMessage=function(){
+
+        var newMessage={
+          message:$scope.message,
+          from:$scope.mynickname
+        }
+
+        socket.emit('send-message',newMessage);
+        $scope.message='';
+        //$scope.messages.push(newMessage);
+
+
+      };
+
+      $scope.sendLike=function(user){
+        console.log(user);
+
+        var id= lodash.get(user,'socketid');
+        var likeObj={
+          from:nickname,
+          like:id
+        };
+
+        socket.emit('send-like',likeObj);
+
+
+      };
+
+
+
+      $scope.joinPrivate=function(){
+
+        socket.emit('join-private',{
+          nickname:nickname
+
+        });
+
+        console.log('private room joined');
+      };
+
+      $scope.groupPm=function(){
+        
+                socket.emit('private-chat',{
+                  message:'hello everyone'
+        
+                });        
+                
+              };
+
+      
     };
   })();
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var server =  require('http').Server(app);
 var io = require('socket.io')(server);
 var port = 8256;
 
+var users=[];
+
 app.use(express.static(path.join(__dirname,"public")));
 app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
 
@@ -23,6 +25,65 @@ io.on('connection',function(socket){
     // socket.on('message-from-client',function(msg){
     //     console.log(msg);
     // });
+
+
+    socket.on('get-users',function(){
+        socket.emit('all-users',users);
+    })
+
+    socket.on('join',function(data){
+        console.log(data);
+        console.log(users);
+        socket.nickname=data.nickname;
+        users[socket.nickname]=socket;
+
+        var userObj={
+            nickname:data.nickname,
+            socketid:socket.id
+        };
+
+        users.push(userObj);
+        io.emit('all-users',users);
+    });
+
+
+    socket.on('send-message',function(data){
+        //socket.broadcast.emit('message-recieved',data);
+        io.emit('message-recieved',data);
+    });
+
+    socket.on('send-like',function(data){
+        console.log(data);
+        
+        socket.broadcast.to(data.like).emit('user-liked',data);
+    });
+
+    socket.on('disconnect',function(data){
+       
+        users=users.filter(function(item){
+            return item.nickname!==socket.nickname;
+        });
+
+        io.emit('all-users',users);
+
+    });
+
+    socket.on('join-private',function(data){
+        //console.log(data);
+        
+        socket.join('private');
+
+        console.log(data.nickname+ "joined private");
+    });
+
+    socket.on('private-chat',function(data){
+        //console.log(data);
+        
+        socket.broadcast.to('private').emit('show-message',data.message);
+        
+    });
+
+    
 
 });
 
